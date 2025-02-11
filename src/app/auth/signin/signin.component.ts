d776fda7a3ec1b74442e5bd07e87d8602392081f
citeFred <citefred@gmail.com>
2025-02-11 12:36:22 +0900

    feat: 로그인 기능 구현(응답헤더의 JWT 로컬스토리지 저장)

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -10,15 +12,30 @@ export class SigninComponent implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {}
 
-  onSignIn() {
+  async onSignIn() {
     const signInData = {
       email: this.email,
       password: this.password,
     }
     console.log('signInData:', signInData);
+
+    try {
+      const response = await this.authService.signIn(signInData);
+      if (response.success) {
+        console.log(response);
+        this.router.navigate(['/']);
+      } else {
+        console.log('Sign In Failed', response.message);
+      }
+    } catch (error) {
+      console.log('Sign In Error', error);
+    }
   }
 }
